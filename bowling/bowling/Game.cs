8a81552da3e87b59a7e6a8d8695ae44c8f50e1fd
xbc1207a <xbc1207a@gmail.com>
2015-10-26 11:26:22 +0800

    commit 2

--- bowling/bowling/Game.cs
+++ bowling/bowling/Game.cs
@@ -7,22 +7,53 @@ namespace bowling
 {
     class Game
     {
-        private int pin = 0;
-        private int score = 0;
-        private bool allHit = false;
+        private int pin, pin1, pin2 = 0;
+        private int temScore = 0;
+        private int finalScore = 0;
+        private int count = 0;
+        private bool firstStrike = false;
+        private bool secondStrike = false;
 
         public void SetPins(int p)
         {
-            if (p == 10) allHit = true;
-            this.pin = p;
+            count++;
+            if (count % 2 == 1)
+            {
+                pin1 = p;
+                LastTime();
+                if (pin1 == 10) firstStrike = true;
+            }
+            else
+            {
+                pin2 = p;
+            }
+            if (pin1 + pin2 == 10 && pin2 != 0 ) secondStrike = true;
+            if (pin1 == 10 && count % 2 == 1) firstStrike = true;
+            if (count % 2 == 0) PinToScore();
+        }
+        public void LastTime()
+        {
+            if (firstStrike == true) finalScore = finalScore + pin1;
+ 
         }
         public int GetFinalScore()
         {
-            return this.score;
+            return this.finalScore;
         }
-        private int PinToScore()
+        private void PinToScore()
         {
-            if(allHit)
+            pin = pin1 + pin2;
+            temScore = temScore + pin; 
+            if (firstStrike)
+            {
+                finalScore = finalScore + pin;
+                firstStrike = false;
+            }
+            if (secondStrike)
+            {
+                finalScore = finalScore + pin1;
+            }
+            finalScore = finalScore + pin;
         }
     }
 }
